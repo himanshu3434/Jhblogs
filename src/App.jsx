4656e3a4dbdbd0c2f3cdd65cbfda4024233000c3
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-01-31 16:19:08 +0530

    Setup initial Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import config from "./config/config";
+function App() {
+  // console.log(config.appwriteUrl);
+  // console.log(config.appwriteDatabaseId);
+  // console.log(config.appwriteProjectId);
+  // console.log(config.appwriteCollectionId);
+  // console.log(config.appwriteBucketId);
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
