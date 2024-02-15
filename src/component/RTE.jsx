587786b7590dffec605509e5d10e7674d1d9fb29
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-02-15 13:56:45 +0530

    created a component for real time editor from tinymce

---
+++ src/component/RTE.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+
+function RTE({ name, controls, label, defaultValue = "" }) {
+  return (
+    <div>
+      {label && <label className="">{label}</label>}
+      <Controller
+        name={name || "content"}
+        controls={controls}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}
+
+export default RTE;
