f10d24183cf2ca902261e068140faf263b73778b
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-02-27 15:35:16 +0530

    Changed Copyright Content

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
               </div>
               <div>
                 <p className="text-sm  ">
-                  &copy; Copyright 2023. All Rights Reserved by DevUI.
+                  &copy; Copyright . All Rights Reserved by Jhblogs.
                 </p>
               </div>
             </div>
